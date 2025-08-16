9ffcb6a69ce37e4e98064b56761814e5d4778457
2127541239@qq.com <lkh2127541239@gmail.com>
2025-08-16 20:30:02 +0800

    Update mp3list.js

--- mp3list.js
+++ mp3list.js
@@ -1,10 +1,18 @@
-// 所有文件夹下的MP3文件列表
+// 所有文件夹下的MP3文件列表（补充你的文件）
 const allMp3Files = [
     "范特西/周杰伦－忍者.mp3",
     "范特西/周杰伦－简单爱.mp3",
     "七里香/周杰伦－七里香.mp3",
     "七里香/周杰伦－借口.mp3",
     "十一月的萧邦/周杰伦 - 发如雪.mp3",
-    "十一月的萧邦/周杰伦 - 漂移.mp3",
-    // 可以继续添加其它文件夹/文件
+    "十一月的萧邦/周杰伦 - 漂移.mp3"
 ];
+
+const showAllBtn = document.getElementById('showAllBtn');
+showAllBtn.onclick = function() {
+    playlist = [];
+    playlist.push(...allMp3Files);
+    currentIndex = -1;
+    updateSongList();
+    status.textContent = "已列出所有MP3文件，请点击歌曲播放";
+};
